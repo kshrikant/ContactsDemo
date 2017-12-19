a86021a83407c47bb38e7da63dbe57dd60a7ae58
Shrikant K <shrikant.kanakatti@wwindia.com>
2017-12-19 17:58:40 +0530

    Adding project

---
+++ InnoplexusContactsAssignment/APIClient.swift
@@ -0,0 +1,13 @@
+//
+//  APIClient.swift
+//  InnoplexusContactsAssignment
+//
+//  Created by Shrikant Kanakatti on 12/18/17.
+//  Copyright © 2017 Shrikant Kanakatti. All rights reserved.
+//
+
+import UIKit
+
+class APIClient: NSObject {
+
+}
